da917ae7fc82fac05987fa5178593f2e4c54f94f
KamisettyLokesh <lokesh6167@gmail.com>
2024-06-23 14:50:23 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,4 @@
+
+const heading = React.createElement("div", { id: 'heading', xyz: 'avc' }, [React.createElement('div', { id: 'child' }, "I am child"), React.createElement('div', { id: 'child2' }, "I am child2")]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
