b630ab23c7ef9e722f982da943ac4b93e6067f4e
KamisettyLokesh <lokesh6167@gmail.com>
2024-06-23 16:56:56 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 
+import React from "react";
+import ReactDOM from 'react-dom';
+
 const heading = React.createElement("div", { id: 'heading', xyz: 'avc' }, [React.createElement('div', { id: 'child' }, "I am child"), React.createElement('div', { id: 'child2' }, "I am child2")]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
---
+++ README.md
@@ -0,0 +1,10 @@
+# Namaste React by Kamisetty Lokesh
+
+# Parcel
+- Bundles
+- Local server
+- HMR
+- Image compression
+- Compression
+- Tree shaking
+- many more
